93edee2f7c366a9bd57082474c1cbd08089fb1fd
shind07 <shind@sfu.ca>
2018-11-23 15:15:22 -0800

    Compress output

--- etl/parse_box_score.py
+++ etl/parse_box_score.py
@@ -68,8 +68,7 @@ def main(output):
         '3FG', '3FGA', 'FT', 'FTA', 'PTS', 'ORebs', 'DRebs', 'Tot Reb', 'AST', 'TO', 'STL', \
         'BLK', 'Fouls',  'opp_Team', 'opp_FGM', 'opp_FGA', 'opp_3FG', 'opp_3FGA', 'opp_FT', 'opp_FTA',\
         'opp_PTS', 'opp_ORebs', 'opp_DRebs', 'opp_Tot Reb', 'opp_AST', 'opp_TO', 'opp_STL', 'opp_BLK', 'opp_Fouls', ]
-    full_data.select(final_columns).write.csv(output, mode='overwrite', header=True)
-
+    full_data.select(final_columns).write.csv(output, mode='overwrite', header=True, compression='gzip')
 
 if __name__ == '__main__':
     output = sys.argv[1]
