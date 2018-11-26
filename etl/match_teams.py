1e59fd575540ddbbde8b652f9d6dd31fce4341d8
shind07 <shind@sfu.ca>
2018-11-25 16:38:18 -0800

    Test matching team names with wikipedia data

--- etl/match_teams.py
+++ etl/match_teams.py
@@ -1,6 +1,12 @@
-from resources import csvToArray, match
+from resources import csvToArray, matchStrings
 import sys
 
+def parseSchoolNames(school):
+    words_to_remove = ['University of', 'College of', 'College', 'University']
+    for w in words_to_remove:
+        school = school.replace(w, '')
+    school = school.replace(' and ', ' & ')
+    return school.replace('  ', ' ').strip()
 def main(teams_path, match_path):
 
     teams = csvToArray(teams_path)
@@ -8,7 +14,16 @@ def main(teams_path, match_path):
 
     for t in teams:
         team = t[0]
-        print(team)
+        max_score = 0
+        matched_team = ''
+        for m in matches:
+            match = parseSchoolNames(m[0])
+            score = matchStrings(team, match, 3)
+            if score > max_score:
+                max_score = score
+                matched_team = match
+        print(team, matched_team, max_score)
+
 
 
 if __name__ == '__main__':
--- etl/resources.py
+++ etl/resources.py
@@ -1,3 +1,5 @@
+import csv
+
 '''
 This file contains any variables/functions/etc that are used in multiple files.
 '''
@@ -17,10 +19,10 @@ box_score_columns = ['Gender', 'Year', 'Division','Date', 'Time', 'Team', 'FGM',
         'BLK', 'Fouls',  'opp_Team', 'opp_FGM', 'opp_FGA', 'opp_3FG', 'opp_3FGA', 'opp_FT', 'opp_FTA',\
         'opp_PTS', 'opp_ORebs', 'opp_DRebs', 'opp_Tot Reb', 'opp_AST', 'opp_TO', 'opp_STL', 'opp_BLK', 'opp_Fouls', ]
 
-def match(str1, str2, degree=1):
+def matchStrings(str1, str2, degree=1):
     min_length = min(len(str1), len(str2))
     degree = min_length if degree > min_length else degree
-    print("Degree: ", degree)
+    #print("Degree: ", degree)
     set1, set2 = set(), set()
 
     def _setify(str, size):
@@ -29,7 +31,7 @@ def match(str1, str2, degree=1):
     for i in range(degree):
         set1 = set1 | _setify(str1, i+1)
         set2 = set2 | _setify(str2, i+1)
-    print(set1, set2)
+    #print(set1, set2)
     max_length = max(len(set1), len(set2))
     return len(set1 & set2) / float(max_length)
 
